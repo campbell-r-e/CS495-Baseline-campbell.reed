cdd2a8ea5bbc9424bf01528adcdaed252a89bb0c
bsucs <122378640+bsucs@users.noreply.github.com>
2025-08-24 19:39:46 -0400

    Added a testing files in order to the lowercase conversion python function.

---
+++ test_util.py
@@ -0,0 +1,17 @@
+import unittest
+import utils
+
+class Testutils(unittest.TestCase):
+    def test_utils1(self):
+        self.assertEqual(utils.To_lowercase("two"),"two")
+      
+    def test_utils2(self):
+       
+        self.assertEqual(utils.To_lowercase("THREE"),"three")
+    def test_utils3(self):
+        
+        self.assertEqual(utils.To_lowercase("fOur"),"four")
+       
+
+       
+
---
+++ utils.py
@@ -0,0 +1,9 @@
+def main():
+    result =To_lowercase("TEST")
+    print(result)
+
+def To_lowercase(parameters):
+    result=parameters.lower()
+    return result
+if __name__ == "__main__":
+    main()
